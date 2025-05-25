8d8e184ebe87a4a7006247faaee1af1fd8b2ad57
xsanfz <sergey.myamin@gmail.com>
2025-05-25 22:27:25 +0400

    Добавил функцию get_telegram_config

--- telegram_tools.py
+++ telegram_tools.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 from typing import Optional
+import os
 
 from telegram import Bot
 from telegram.error import TelegramError
+
+
+def get_telegram_config():
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_channel = os.getenv('TELEGRAM_CHANNEL')
+    return telegram_token, telegram_channel
 
 
 async def publish_photo(bot: Bot, channel: str, photo_path: Path) -> bool:
